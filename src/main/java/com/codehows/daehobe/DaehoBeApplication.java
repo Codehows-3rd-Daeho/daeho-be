127ac570e84314daa29ad90a4aaf57c82e86a82d
박단현 <166380533+sodanhyun@users.noreply.github.com>
2025-11-21 16:20:25 +0900

    Initial Commit

---
+++ src/main/java/com/codehows/daehobe/DaehoBeApplication.java
@@ -0,0 +1,13 @@
+package com.codehows.daehobe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaehoBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DaehoBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codehows/daehobe/DaehoBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codehows.daehobe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaehoBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
